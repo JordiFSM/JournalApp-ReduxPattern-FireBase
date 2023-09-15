b9936df0a63ac1eaa68227ab21cae6f1d84876b0
Jordi <jordifabiansegura@gmail.com>
2023-09-15 15:17:54 -0600

    develop of crud first part: insert and update, aditionali adding view interaction with the users, like succed alert when user save notes

--- src/journal/view/NoteView.jsx
+++ src/journal/view/NoteView.jsx
@@ -1,9 +1,41 @@
 import { SaveOutlined } from '@mui/icons-material';
 import { Button, Grid, TextField, Typography } from '@mui/material';
 import { ImageGallery } from '../components'
+import { useForm } from '../../hooks';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useMemo } from 'react';
+import { setActiveNote, startSaveNotes } from '../../store/journal';
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.css'
 
 
 export const NoteView = () => {
+
+    const dispatch = useDispatch();
+    const { active: note, messageSaved, isSaving} = useSelector( state => state.journal );
+    const { body, title, date, onInputChange, formState } = useForm( note );
+
+    const dateString = useMemo(() => {
+        const newDate = new Date(date);
+        return newDate.toUTCString();
+    }, [date])
+
+    useEffect(() => {
+        dispatch(setActiveNote(formState))
+    }, [formState])
+
+    useEffect(() => {
+      if(messageSaved.length > 0){
+        Swal.fire('Nota actualizada', messageSaved, 'success');
+      }
+    }, [messageSaved])
+    
+
+    const onSaveNote = () => {
+        dispatch( startSaveNotes() )
+    }
+    
+
   return (
     <Grid 
         container 
@@ -14,10 +46,14 @@ export const NoteView = () => {
         className='animate__animated animate__fadeIn animate__faster'
     >
         <Grid item>
-            <Typography fontSize={ 39 } fontWeight='light' >28 de agosto, 2023</Typography>
+            <Typography fontSize={ 39 } fontWeight='light' >{ dateString }</Typography>
         </Grid>
         <Grid item>
-            <Button color="primary" sx={{ padding: 2 }}>
+            <Button 
+                disabled={ isSaving }
+                onClick={ onSaveNote }
+                color="primary" 
+                sx={{ padding: 2 }}>
                 <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
                 Guardar
             </Button>
@@ -31,6 +67,9 @@ export const NoteView = () => {
                 placeholder="Ingrese un título"
                 label="Título"
                 sx={{ border: 'none', mb: 1 }}
+                name='title'
+                value={ title }
+                onChange={ onInputChange }
             />
 
             <TextField 
@@ -40,6 +79,9 @@ export const NoteView = () => {
                 multiline
                 placeholder="¿Qué sucedió en el día de hoy?"
                 minRows={ 5 }
+                name='body'
+                value={ body }
+                onChange={ onInputChange }
             />
         </Grid>
 
